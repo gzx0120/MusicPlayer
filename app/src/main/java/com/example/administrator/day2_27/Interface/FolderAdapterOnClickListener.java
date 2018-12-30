adf004bc68997df0812e6a4769ddb05e38fd3499
gzx0120 <350022282@qq.com>
2018-12-30 17:57:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/day2_27/Interface/FolderAdapterOnClickListener.java
@@ -0,0 +1,12 @@
+package com.example.administrator.day2_27.Interface;
+
+import java.io.File;
+
+
+public interface FolderAdapterOnClickListener {
+    void insideStorage(File file);
+
+    void externalStorage();
+
+    void normalClick(File file);
+}
